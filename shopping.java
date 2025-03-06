ea8d39864fb3be0d65a5ca97c1f9d6683267d1ee
hemant marothi <hemant.marothi@metacube.com>
2025-03-06 14:01:48 +0530

    comments and updates

--- shopping.java
+++ shopping.java
@@ -3,17 +3,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class shopping{
 
     public static void itemsMenu(ArrayList<Item> items){
-        // A Menu of all the items present.
-        System.out.println("\n\n===============================================================================================");
+        // A Menu of all the items present in the shop.
+        System.out.println("\n\n==============================================================================");
         System.out.println(String.format("|%-8s|%-15s|%-35s|%-15s|","Item Id", "Item Name", "Item Desc", "Item Price"));
-        System.out.println("===============================================================================================");
+        System.out.println("==============================================================================");
         for(Item i : items){
             System.out.println(String.format("|%-8d|%-15s|%-35s|%-15.2f|",i.getId(), i.getName(), i.getDescription(), i.getPrice()));
         }
-        System.out.println("===============================================================================================");
+        System.out.println("==============================================================================");
     }
 
     public static void operationsMenu(){
@@ -25,14 +26,17 @@ public class shopping{
         System.out.println("4. Delete Item");
         System.out.println("5. Display Bill");
         System.out.println("6. Exit");
+        System.out.println("#######################################");
     }
 
     public static void main(String[] args){
-        int id, quant;
+
+        int item_id;
+        int item_quantity;
         Scanner sc = new Scanner(System.in);
-        ArrayList<Item> items = new ArrayList<>();
 
         // Items created in memory
+        ArrayList<Item> items = new ArrayList<>();
         items.add(new Item("Honda", "Bike", 100.0));
         items.add(new Item("Dairymilk", "toffee", 10.2));
         items.add(new Item("Milk", "Source of Calcium", 40.0));
@@ -44,7 +48,7 @@ public class shopping{
         items.add(new Item("Dahi", "Source of Calcium", 40.0));
 
         //A cart object for the customer
-        shoppingCart shop = new shoppingCart();
+        ShoppingCart shop_cart = new ShoppingCart();
         boolean flag = true;
 
         // Will run until exited by customer
@@ -57,53 +61,73 @@ public class shopping{
             switch(choice){
                 case 1: 
                     itemsMenu(items);
-                    System.out.print("Enter the item ID You Want to add: ");
-                    id = sc.nextInt();
-                    System.out.print("Enter the Quantity of that item You Want to add: ");
-                    quant = sc.nextInt();
-                    shop.addToCart(items.get(id-1), quant);
+                    try {
+                        System.out.print("Enter the item ID You Want to add: ");
+                        item_id = sc.nextInt();
+                        System.out.print("Enter the Quantity of that item You Want to add: ");
+                        item_quantity = sc.nextInt();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        break;
+                    }
+                    
+                    shop_cart.addToCart(items.get(item_id-1), item_quantity);
                     break;
 
                 case 2:
-                    shop.displayQty();
+                    shop_cart.displayQty();
                     break;
 
                 case 3:
                     itemsMenu(items);
-                    System.out.print("Enter the item ID You Want to update: ");
-                    id = sc.nextInt();
-                    System.out.print("Enter the Quantity by how much You Want to update: ");
-                    quant = sc.nextInt();
-                    shop.updateQty(items.get(id-1), quant);
+                    try {
+                        System.out.print("Enter the item ID You Want to update: ");
+                        item_id = sc.nextInt();
+                        System.out.print("Enter the Quantity by how much You Want to update: ");
+                        item_quantity = sc.nextInt();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                        break;
+                    }
+                    shop_cart.updateQty(items.get(item_id-1), item_quantity);
                     break;
 
                 case 4:
-                    itemsMenu(items);
-                    System.out.print("Enter the item ID You Want to delete: ");
-                    id = sc.nextInt();
-                    shop.deleteItem(items.get(id-1));
+                    shop_cart.displayQty();
+                    try{
+                        System.out.print("Enter the item ID You Want to delete: ");
+                        item_id = sc.nextInt();
+                    }catch(Exception e){
+                        System.out.println(e);
+                        break;
+                    }
+                    shop_cart.deleteItem(items.get(item_id-1));
                     break;
                 
                 case 5:
-                    System.out.println("Total Bill for above items: " + shop.displayBill() + "\n\n");
+                    System.out.println("Total Bill for above items: " + shop_cart.displayBill() + "\n\n");
                     break;
 
                 case 6:
                     flag = false;
                     break;
                     
-                default: System.out.println("Choose Correct option.");
+                default: System.out.println("!! Invalid input. Please Choose Correct option.");
             }
             
         }
     }
 }
 
-class shoppingCart{
+
+
+
+
+class ShoppingCart{
     private Map<Item, Integer> cart = new HashMap<>();
     
     /** 
-       Adds the item with provided quantity. Confirms if item is added or warns if item already in cart.   
+       *Adds the item with provided quantity into the cart.
     */
     public void addToCart(Item item, Integer quantity){
         
@@ -115,9 +139,8 @@ class shoppingCart{
         System.out.println("--> " + item.getName() + " added to cart\n\n");
     }
 
-
     /** 
-       Displays the item and its corresponding quantity added in the cart.
+       *Displays the item and its corresponding quantity added in the cart.
     */
     public void displayQty(){
         if(cart.isEmpty()){
@@ -125,16 +148,15 @@ class shoppingCart{
             return;
         }
         for(Map.Entry<Item, Integer> entry : cart.entrySet()){
-            System.out.println("=====================");
-            System.out.println("| " + entry.getKey().getName()+"  |  " +entry.getValue() + "   |");
-            System.out.println("=====================");
+            System.out.println("=================================");
+            System.out.println("| " + entry.getKey().getId() + " |"+ entry.getKey().getName()+"  |  " +entry.getValue() + "   |");
+            System.out.println("=================================");
         }
         System.out.println("\n\n");
     }
 
-
     /** 
-       Updates the quantity of item in the cart by increasing or decreasing the amount.
+       *Updates the quantity of item in the cart by increasing or decreasing the amount.
     */
     public void updateQty(Item item,Integer quantity){
         if(cart.getOrDefault(item, 0) + quantity >= 0){
@@ -147,7 +169,7 @@ class shoppingCart{
     }
 
     /** 
-       Deletes the provided item if present in cart.
+       *Deletes the provided item if present in cart.
     */
     public void deleteItem(Item item){
         if(cart.containsKey(item)){
@@ -159,9 +181,8 @@ class shoppingCart{
         
     }
 
-
     /** 
-       Calculates and return the total bill of the cart.
+       *Calculates and return the total bill of the cart.
     */
     public Double displayBill(){
         Double total = 0.0;
@@ -169,22 +190,29 @@ class shoppingCart{
             total += entry.getKey().getPrice() * entry.getValue();
         }
         return total;
-    } 
-
+    }
 }
+
+
+
+
+
 class Item{
-    static Integer id=1;
+
+    private static Integer unique_id=1;
     private Integer ItemId;
     private Double Price;
     private String Name;
     private String Description;
     
     public Item(String Name, String Description, Double Price){
-        this.ItemId = id++;
+        this.ItemId = unique_id++;
         this.Name = Name;
         this.Description = Description;
         this.Price = Price;
     }
+
+
     public Integer getId(){
         return ItemId;
     }
